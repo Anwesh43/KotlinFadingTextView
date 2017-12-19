3d6bac3a76b6e810a1c54653d6f2405e451dd600
Anwesh43 <anweshthecool0@gmail.com>
2017-12-19 21:53:53 +0530

    using FadingTextView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinplusfadingtextview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinplusfadingtextview/MainActivity.kt
@@ -2,11 +2,15 @@ package ui.anwesome.com.kotlinplusfadingtextview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.fadingtextview.FadingTextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var view = FadingTextView.create(this)
+        view.setText1("hello world")
+        view.addToParent(this)
     }
 }
