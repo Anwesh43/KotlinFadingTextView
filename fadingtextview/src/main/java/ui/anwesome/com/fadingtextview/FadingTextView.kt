fa6c7d90ae0df479683675e7bf42ecd0bb4793d3
Anwesh43 <anweshthecool0@gmail.com>
2017-12-19 08:04:10 +0530

    adding PlusButton

--- fadingtextview/src/main/java/ui/anwesome/com/fadingtextview/FadingTextView.kt
+++ fadingtextview/src/main/java/ui/anwesome/com/fadingtextview/FadingTextView.kt
@@ -32,5 +32,20 @@ class FadingTextView(ctx:Context):View(ctx) {
             canvas.restore()
         }
     }
-    
+    data class PlusButton(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas: Canvas,paint:Paint,scale:Float) {
+            paint.color = Color.parseColor("#311B92")
+            paint.strokeWidth = r/5
+            paint.strokeCap = Paint.Cap.ROUND
+            for(i in 0..1) {
+                canvas.save()
+                canvas.translate(x,y)
+                canvas.rotate(i*90f*scale)
+
+                canvas.drawLine(0f,-r,0f,r,paint)
+                canvas.restore()
+            }
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+    }
 }
